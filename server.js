61cb9dce3c35e42332adea2110810361e399a2fe
cupshir <christopherupshir@ccv.church>
2019-05-07 23:42:51 -0700

    changed endpoints that return all items from a plural name to endpoint/all

--- server.js
+++ server.js
@@ -16,35 +16,35 @@ app.use(express.json());
 
 // person
 app.post(apiRoot + '/person', PersonController.create);
-app.get(apiRoot + '/people', PersonController.get);
+app.get(apiRoot + '/person/all', PersonController.get);
 app.get(apiRoot + '/person/:id', PersonController.get);
 app.put(apiRoot + '/person/:id', PersonController.update);
 app.delete(apiRoot + '/person/:id', PersonController.delete);
 
 // park
 app.post(apiRoot + '/park', ParkController.create);
-app.get(apiRoot + '/parks', ParkController.get);
+app.get(apiRoot + '/park/all', ParkController.get);
 app.get(apiRoot + '/park/:id', ParkController.get);
 app.put(apiRoot + '/park/:id', ParkController.update);
 app.delete(apiRoot + '/park/:id', ParkController.delete);
 
 // attraction
 app.post(apiRoot + '/attraction', AttractionController.create);
-app.get(apiRoot + '/attractions', AttractionController.get);
+app.get(apiRoot + '/attraction/all', AttractionController.get);
 app.get(apiRoot + '/attraction/:id', AttractionController.get);
 app.put(apiRoot + '/attraction/:id', AttractionController.update);
 app.delete(apiRoot + '/attraction/:id', AttractionController.delete);
 
 // journal
 app.post(apiRoot + '/journal', JournalController.create);
-app.get(apiRoot + '/journals', JournalController.get);
+app.get(apiRoot + '/journal/all', JournalController.get);
 app.get(apiRoot + '/journal/:id', JournalController.get);
 app.put(apiRoot + '/journal/:id', JournalController.update);
 app.delete(apiRoot + '/journal/:id', JournalController.delete);
 
 // journal entry
 app.post(apiRoot + '/journal-entry', JournalEntryController.create);
-app.get(apiRoot + '/journal-entries', JournalEntryController.get);
+app.get(apiRoot + '/journal-entry/all', JournalEntryController.get);
 app.get(apiRoot + '/journal-entry/:id', JournalEntryController.get);
 app.put(apiRoot + '/journal-entry/:id', JournalEntryController.update);
 app.delete(apiRoot + '/journal-entry/:id', JournalEntryController.delete);
